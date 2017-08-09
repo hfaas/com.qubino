e416526d0a8f8d72c13dd4245fe3522ebd085939
spikey <spikey.f@gmail.com>
2017-08-09 21:23:43 +0200

    Bug fix: Corrected parameter size of parameter "dimming time" to 2 based on manual.

--- drivers/ZMNHVD1/driver.js
+++ drivers/ZMNHVD1/driver.js
@@ -106,7 +106,7 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		},
 		dimming_time_when_key_pressed: {
 			index: 66,
-			size: 1,
+			size: 2,
 		},
 		temperature_sensor_offset: {
 			index: 110,
