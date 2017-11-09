3e4a52b3b43137e9eede44f49c81b80f0dd7e162
Robin Bolscher <robinbolscher@gmail.com>
2017-11-09 18:55:01 +0100

    Initial commit SDK rewrite

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const Homey = require('homey');
 const Log = require('homey-log').Log;
 
-module.exports.init = () => {
-	console.log(`${Homey.manifest.id} running...`);
-};
+class QubinoApp extends Homey.App {
+	onInit(){
+		this.log(`${Homey.manifest.id} running...`);
+	}
+}
+
+module.exports = QubinoApp;
--- drivers/ZMNHDD1/driver.js
+++ drivers/ZMNHDD1/driver.js
@@ -1,212 +1,25 @@
 'use strict';
 
-const path = require('path');
-const ZwaveDriver = require('homey-zwavedriver');
+const Homey = require('homey');
 
-// Documentation: http://qubino.com/download/990/
+// Documentation: https://www.benext.eu/static/manual/qubino/flush-1-relay-ZMNHAA2.pdf
 
-module.exports = new ZwaveDriver(path.basename(__dirname), {
-	capabilities: {
-		onoff: [
-			{
-				command_class: 'COMMAND_CLASS_SWITCH_BINARY',
-				command_set: 'SWITCH_BINARY_SET',
-				command_set_parser: value => ({
-					'Switch Value': (value) ? 'on/enable' : 'off/disable',
-				}),
-			},
-			{
-				command_class: 'COMMAND_CLASS_SWITCH_MULTILEVEL',
-				command_get: 'SWITCH_MULTILEVEL_GET',
-				command_report: 'SWITCH_MULTILEVEL_SET',
-				command_report_parser: report => {
-					console.log('report', report)
-					return report['Value'] !== 'off/disable'
-				},
-			}			
-		],
-		dim: {
-			command_class: 'COMMAND_CLASS_SWITCH_MULTILEVEL',
-			command_get: 'SWITCH_MULTILEVEL_GET',
-			command_set: 'SWITCH_MULTILEVEL_SET',
-			command_set_parser: value => ({
-				Value: Math.round(value * 99),
-				'Dimming Duration': 255,
-			}),
-			command_report: 'SWITCH_MULTILEVEL_REPORT',
-			command_report_parser: (report, node) => {
-				if (report.Value === 'on/enable') {
-					module.exports.realtime(node.device_data, 'onoff', true);
-					return 1.0;
-				}
-				else if (report.Value === 'off/disable') {
-					module.exports.realtime(node.device_data, 'onoff', false);
-					return 0.0;
-				}
-				else if (typeof report.Value === 'number') {
-					module.exports.realtime(node.device_data, 'onoff', report.Value > 0);
-					return report.Value / 99;
-				}
-				else if (typeof report['Value (Raw)'] !== 'undefined') {
-					module.exports.realtime(node.device_data, 'onoff', report['Value (Raw)'][0] > 0);
-					if(report['Value (Raw)'][0] === 255) return 1.0;
-					return report['Value (Raw)'][0] / 99;
-				}
-				return null;
-			},
-		},
-		measure_power: {
-			command_class: 'COMMAND_CLASS_METER',
-			command_get: 'METER_GET',
-			command_get_parser: () => ({
-				Properties1: {
-					Scale: 7,
-					'Rate Type': 'Import'
-				},
-				'Scale 2': 1
-			}),
-			command_report: 'METER_REPORT',
-			command_report_parser: report => {
-				if (report.hasOwnProperty('Properties2')
-					&& report.Properties2.hasOwnProperty('Scale bits 10')
-					&& report.Properties2['Scale bits 10'] === 2) {
-					return report['Meter Value (Parsed)'];
-				}
-				return null;
-			},
-		},
-		meter_power: {
-			command_class: 'COMMAND_CLASS_METER',
-			command_get: 'METER_GET',
-			command_get_parser: () => ({
-				Properties1: {
-					Scale: 0,
-					'Rate Type': 'Import'
-				},
-				'Scale 2': 1
-			}),
-			command_report: 'METER_REPORT',
-			command_report_parser: report => {
-				if (report.hasOwnProperty('Properties2')
-					&& report.Properties2.hasOwnProperty('Scale bits 10')
-					&& report.Properties2['Scale bits 10'] === 0) {
-					return report['Meter Value (Parsed)'];
-				}
-				return null;
-			},
-		},
-		measure_temperature: {
-			command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
-			command_get: 'SENSOR_MULTILEVEL_GET',
-			command_get_parser: () => ({
-				'Sensor Type': 'Temperature (version 1)',
-				Properties1: {
-					Scale: 0,
-				},
-			}),
-			command_report: 'SENSOR_MULTILEVEL_REPORT',
-			command_report_parser: report => {
-				if (report['Sensor Value (Parsed)'] === -999.9) return null;
-				return report['Sensor Value (Parsed)'];
-			},
-			optional: true,
-		},
-	},
-	settings: {
-		input_1_type: {
-			index: 1,
-			size: 1,
-		},
-		input_2_type: {
-			index: 2,
-			size: 1,
-		},
-		input_2_contact_type: {
-			index: 3,
-			size: 1,
-		},
-		input_3_contact_type: {
-			index: 4,
-			size: 1,
-		},
-		deactivate_ALL_ON_ALL_OFF: {
-			index: 10,
-			size: 2,
-		},
-		automatic_turning_off_output_after_set_time: {
-			index: 11,
-			size: 2,
-		},
-		automatic_turning_on_output_after_set_time: {
-			index: 12,
-			size: 2,
-		},
-		state_of_device_after_power_failure: {
-			index: 30,
-			size: 1,
-		},
-		power_report_on_power_change: {
-			index: 40,
-			size: 1,
-		},
-		power_report_by_time_interval: {
-			index: 42,
-			size: 2,
-		},
-		maximum_dimming_value: {
-			index: 61,
-			size: 1,
-		},
-		minimum_dimming_value: {
-			index: 60,
-			size: 1,
-		},
-		dimming_time_soft_on_off: {
-			index: 65,
-			size: 1,
-		},
-		dimming_time_when_key_pressed: {
-			index: 66,
-			size: 2,
-		},
-	},
-});
+class ZMNHDD1 extends Homey.Driver {
 
-module.exports.on('initNode', token => {
-	const node = module.exports.nodes[token];
-	
-	console.log('node && node.instance && node.instance.MultiChannelNodes', node.instance.MultiChannelNodes)
+	onInit() {
 
-	if (node && node.instance && node.instance.MultiChannelNodes ) {
-		
-		// I2 switched
-		if (node.instance.MultiChannelNodes['2'] && node.instance.MultiChannelNodes['2'].CommandClass.COMMAND_CLASS_SENSOR_BINARY) {
-			node.instance.MultiChannelNodes['2'].CommandClass.COMMAND_CLASS_SENSOR_BINARY.on('report', (command, report) => {
-				console.log('2', 'command, report', command, report)
-				if (command.name === 'SENSOR_BINARY_REPORT') {
-					if (report['Sensor Value'] === 'detected an event') {
-						Homey.manager('flow').triggerDevice('ZMNHDD1_I2_on', {}, {}, node.device_data);
-					} else if (report['Sensor Value'] === 'idle') {
-						Homey.manager('flow').triggerDevice('ZMNHDD1_I2_off', {}, {}, node.device_data);
-					}
-				}
-			});
-		}
-
-		// I3 switched
-		if (node.instance.MultiChannelNodes['3'] && node.instance.MultiChannelNodes['3'].CommandClass.COMMAND_CLASS_SENSOR_BINARY) {
-			node.instance.MultiChannelNodes['3'].CommandClass.COMMAND_CLASS_SENSOR_BINARY.on('report', (command, report) => {
-				console.log('3', 'command, report', command, report)
-				if (command.name === 'SENSOR_BINARY_REPORT') {
-					if (report['Sensor Value'] === 'detected an event') {
-						Homey.manager('flow').triggerDevice('ZMNHDD1_I3_on', {}, {}, node.device_data);
-					} else if (report['Sensor Value'] === 'idle') {
-						Homey.manager('flow').triggerDevice('ZMNHDD1_I3_off', {}, {}, node.device_data);
-					}
-				}
-			});
-		}
-	} else {
-		console.log('Failed to get multiChannelNodes for ZMNHDD1');
+		// Register input 2 flow card triggers
+		this.flowTriggerDeviceMultiChannelNode = {
+			1: {
+				on: new Homey.FlowCardTriggerDevice('ZMNHDD1_I2_on').register(),
+				off: new Homey.FlowCardTriggerDevice('ZMNHDD1_I2_off').register(),
+			},
+			2: {
+				on: new Homey.FlowCardTriggerDevice('ZMNHDD1_I3_on').register(),
+				off: new Homey.FlowCardTriggerDevice('ZMNHDD1_I3_off').register(),
+			},
+		};
 	}
-});
+}
+
+module.exports = ZMNHDD1;
