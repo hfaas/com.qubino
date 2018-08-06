dc0a4af844e76849f23da5e0955c471c3f47fbf3
Robin Bolscher <robinbolscher@gmail.com>
2018-08-06 11:28:02 +0200

    Rename in device.js

--- drivers/ZMNHYD/device.js
+++ drivers/ZMNHYD/device.js
@@ -4,7 +4,7 @@ const constants = require('../../lib/constants');
 const QubinoDevice = require('../../lib/QubinoDevice');
 
 /**
- * Flush 1 Relay (ZMNHYD)
+ * Smart Plug (ZMNHYD)
  * Extended manual: http://qubino.com/download/2302/
  * TODO: images/icons
  */
