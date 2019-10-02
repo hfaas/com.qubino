6ae19b5e51900e550398d0960b01d4905d3f4df1
Robin Bolscher <robinbolscher@gmail.com>
2019-10-02 15:31:14 +0200

    Adds TODOs

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Log = require('homey-log').Log;
  * TODO: mark devices as offline while fetching settings etc.
  * TODO: switch type settings reference to parameter 20/100
  * TODO: remove temp capability if temperature sensor is not connected
+ * TODO: remove migration logic after this version has been stable for a while
+ * TODO: constants in settings migrations
  */
 class QubinoApp extends Homey.App {
 	onInit() {
